197903e2804e4671075b06930e5e58ece5b0bd9f
rafaelsperling <rafaelsperling@gmail.com>
2014-02-12 15:10:39 -0200

    Add Card Domain

--- trello-dj/example/card.py
+++ trello-dj/example/card.py
@@ -2,19 +2,6 @@ import requests
 
 # Create your models here.
 
-class example:
-	def __init__(self, name, id, desc):
-		self.name = name
-		self.id = id
-		self.desc = desc
-
-class exampleDao:
-
-	def find(self):
-		r = requests.get('http://www.mocky.io/v2/52f0464f1990e56906990d8f')	
-		js = r.json()
-		return example(js[u'name'], js[u'id'], js[u'desc'])
-			
 class card:
 	def __init__(self, name, id, points, startDate, endDate):
 		self.name = name
@@ -26,7 +13,7 @@ class card:
 class cardDao:
 
 	def find(self):
-		r = requests.get('http://127.0.0.1:7070/api/v1/teams/boards/1/queues/1/cards/1')	
+		r = requests.get('http://localhost:7070/api/v1/teams/boards/1/queues/1/cards/1')	
 		js = r.json()
 		return example(js[u'name'], js[u'id'], js[u'points'], js[u'startDate'], js[u'endDate'])
 			
--- trello-dj/example/views.py
+++ trello-dj/example/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render_to_response
 from models  import *
 
 def hello(request):
-	example = exampleDao().find()
-        return render_to_response('templates/example.html', {'example' : example})
+	example = cardDao().find()
+        return render_to_response('templates/example.html', {'card' : card})
