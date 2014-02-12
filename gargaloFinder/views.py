b5439de55f2bdf27e829089fcc52a6b7e0ea9e31
rafaelsperling <rafaelsperling@gmail.com>
2014-02-12 18:08:59 -0200

    busca nos cards para ver quando não teve code review

--- gargaloFinder/views.py
+++ gargaloFinder/views.py
@@ -6,11 +6,18 @@ def teste(request):
 	boards = json.load(urllib2.urlopen("http://10.99.3.56:7070/api/v1/teams/boards"))
 	bordJson = []
 	for b in boards:
-		board = json.load(urllib2.urlopen("http://10.99.3.56:7070/api/v1/teams/boards/"+b[ b.rindex('/')+1 ]));
+		board = json.load(urllib2.urlopen(b.replace('localhost','10.99.3.56')))
 		bordJson.append(board)
 		for ee in board['queues'] :
-			print(ee);
-		
+			cards = json.load(urllib2.urlopen(ee.replace('localhost','10.99.3.56')))
+			for c in cards['cards']:
+				card = json.load(urllib2.urlopen(c.replace('localhost','10.99.3.56')))
+				codeReview = card['leadTime']['codeReview']
+				if int(codeReview) == 0 and card['endDate'] != '':
+					print('Nao teve codeReview')
+
+
+			
 
 
 	return render_to_response('index.html', locals())
